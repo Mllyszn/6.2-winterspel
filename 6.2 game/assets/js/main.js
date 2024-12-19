300371c611af48e4783ff9497ae254afc43b78e9
Mllyszn <melvinlieuw121107@gmail.com>
2024-12-19 14:18:54 +0100

    1st commit 'intro'

---
+++ 6.2 game/assets/js/main.js
@@ -0,0 +1,11 @@
+kaboom();
+
+function myFunction() {
+  document.body.style.background = "#f3f3f3 url('/PNG/BG_01/BG_01.png')";
+}
+
+// load a sprite "bean" from an image
+loadSprite("slime", "img/slime.png");
+
+// add something to screen
+add([sprite("slime"), pos(80, 40)]);
